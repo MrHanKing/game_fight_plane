132006fb8228e498e82101faaf0a91cd63d80069
hanjun <hanjun@git.com>
2017-11-16 13:57:45 +0800

    修正了个错误

--- scene/title/scene_title.js
+++ scene/title/scene_title.js
@@ -8,6 +8,6 @@ class SceneTitle extends GuaScene {
     }
     draw() {
         // draw labels
-        // this.game.context.fillText('按 k 开始游戏', 100, 190)
+        this.game.context.fillText('按 k 开始游戏', 100, 190)
     }
 }
